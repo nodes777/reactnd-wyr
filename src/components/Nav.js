4f1f734b6241b4afe216a15b4d14f8d4568b289a
Taylor <taylor.nodell@gmail.com>
2019-02-12 12:02:17 -0500

    solves nav component props issue

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,37 @@
 import React, { Component } from "react";
-
+import { connect } from "react-redux";
 import { NavLink } from "react-router-dom";
 
-export default function Nav() {
-	return (
-		<nav className="nav">
-			<ul>
-				<li>
-					<NavLink to="/" exact>
-						Home
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to="/new">New Question</NavLink>
-				</li>
-				<li>
-					<NavLink to="/leaderboard">Leaderboard</NavLink>
-				</li>
-			</ul>
-			{/* if logged in, render the profile link, and logout button here*/}
-		</nav>
-	);
+class Nav extends Component {
+	componentDidMount() {
+		console.log(this.props);
+	}
+	render() {
+		return (
+			<nav className="nav">
+				<ul>
+					<li>
+						<NavLink to="/" exact>
+							Home
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to="/new">New Question</NavLink>
+					</li>
+					<li>
+						<NavLink to="/leaderboard">Leaderboard</NavLink>
+					</li>
+				</ul>
+				{/* if logged in, render the profile link, and logout button here*/}
+			</nav>
+		);
+	}
 }
+
+function mapStateToProps({ authedUser }) {
+	return {
+		loading: authedUser === null
+	};
+}
+
+export default connect(mapStateToProps)(Nav);
