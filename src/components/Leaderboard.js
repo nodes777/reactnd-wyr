b563bf5432be32c3e8326cd28e547987066f4009
Taylor <taylor.nodell@gmail.com>
2019-02-17 11:26:52 -0500

    sorts the leaderboard by score

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,39 +1,54 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 
+import { byScore } from "../utils/utils";
+
 class Leaderboard extends Component {
 	componentDidMount() {}
 
 	render() {
 		const { users } = this.props;
-		console.log(users);
 		return (
 			<div>
-				{users.allIds.map(user => {
-					const currentUser = users[user];
-					// console.log(currentUser);
-					// console.log(users);
-					const asked = currentUser.questions.length;
-					const answered = Object.keys(currentUser.answers).length;
-					const score = asked + answered;
-					return (
-						<div key={user}>
-							<h2>{currentUser.name}</h2>
-							<div>Asked {asked}</div>
-							<div>Answered {answered}</div>
-							<div>Score {score}</div>
-						</div>
-					);
-				})}
+				{users.allIds
+					// is there a better way to refactor this?
+					// if I try to import a sort(byScore) function from utils, I lose access to the users object
+					.sort((a, b) => {
+						const userA = users[a];
+						const askedA = userA.questions.length;
+						const answeredA = Object.keys(userA.answers).length;
+						const scoreA = askedA + answeredA;
+
+						const userB = users[b];
+						const askedB = userB.questions.length;
+						const answeredB = Object.keys(userB.answers).length;
+						const scoreB = askedB + answeredB;
+
+						return scoreB - scoreA;
+					})
+					.map(userId => {
+						const currentUser = users[userId];
+						const asked = currentUser.questions.length;
+						const answered = Object.keys(currentUser.answers)
+							.length;
+						const score = asked + answered;
+						return (
+							<div key={userId}>
+								<h2>{currentUser.name}</h2>
+								<div>Asked {asked}</div>
+								<div>Answered {answered}</div>
+								<div>Score {score}</div>
+							</div>
+						);
+					})}
 			</div>
 		);
 	}
 }
 
-function mapStateToProps({ users, questions }) {
+function mapStateToProps({ users }) {
 	return {
-		users: users,
-		questions: questions
+		users: users
 	};
 }
 
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -10,3 +10,20 @@ export const byTimeStamp = (a, b) => {
 	}
 	return comparison;
 };
+
+export const byScore = (users, a, b) => {
+	console.log(users);
+	console.log(a);
+	const scoreA =
+		users[a].questions.length + Object.keys(users[a].answers).length;
+	const scoreB =
+		users[b].questions.length + Object.keys(users[b].answers).length;
+
+	let comparison = 0;
+	if (scoreA > scoreB) {
+		comparison = 1;
+	} else if (scoreA < scoreB) {
+		comparison = -1;
+	}
+	return comparison;
+};
